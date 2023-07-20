2c2d1c3bb5247b75af1cd93f6fe85b7a0435ee8b
l_loizides <lefteris.loizides@mindgeek.com>
2023-07-21 01:59:50 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
-import {Button, Grid, Paper, Typography, useMediaQuery} from '@material-ui/core';
-import {Twitter, ChatBubble} from '@material-ui/icons'
+import {Grid, Typography, useMediaQuery} from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import { Data, Response } from './types';
 import axios from 'axios'
+import { clsx } from 'clsx';
 
 const options = {
   method: 'GET',
@@ -15,11 +15,12 @@ const options = {
 };
 
 function App() {
-  const [data, setData] = useState<Data | null>(null)
+  const [data, setData] = useState<Data>({} as Data)
   const [loading, setLoading] = useState(false)
   const isMobile = useMediaQuery('(max-width: 600px)')  
 
   const getQuote = async () => {
+    if (loading) return
     setLoading(true)
     try {
       const res = await axios.request(options)
@@ -34,48 +35,29 @@ function App() {
 
   useEffect(() => {
     void getQuote()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  
+
   return (
     <div className='app'>
-      <Grid container spacing={1} id="quote-box">
-        {!isMobile && <Grid item sm={2}/>}
-          <Grid item xs={12} sm={8}>
-            <Paper style={{padding: '20px 10px'}}>
-              <Grid container spacing={1}>
-                <Grid item xs={12}>
-                  <Typography component='p' variant={isMobile ? 'subtitle1' : 'h6'} id="text" align='center'>{data?.quote}</Typography>
-                </Grid>
+      <Grid container id="quote-box" alignContent='space-between' justifyContent='center'>
+        <div>
+          <div className='text-container'>
+            <Typography variant={isMobile ? 'subtitle1' : 'h4'} id="text" align='center'>{data?.quote}</Typography>
+          </div>
+          <div className='text-container'>
+            <Typography variant={isMobile ? 'caption' : 'h6'} id="author">{data?.author && '- ' + data.author}</Typography>
+          </div>
+        </div>
 
-                {!isMobile && <Grid item sm={6}/>}
-                <Grid item xs={12} sm={6}>
-                  <Typography component={'p'} variant={isMobile ? 'caption' : 'subtitle2'} align='right' id="author">- {data?.author}</Typography>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          {!isMobile && <Grid item sm={2}/>}
-
-          {!isMobile && <Grid item sm={2}/>}
-          <Grid item xs={12} sm={4}>
-            <Button onClick={() => void getQuote()} fullWidth variant='contained' id="new-quote" color='primary' disabled={loading}>
-              <ChatBubble fontSize='small'/>
-              &nbsp;
-              <Typography variant={isMobile ? 'caption' : 'body1'}>
-                new quote
-              </Typography>
-            </Button>
-          </Grid>
-
-          <Grid item xs={12} sm={4}>
-            <Button fullWidth variant='contained' style={{backgroundColor: '#1D9BF0'}}>
-              <Twitter fontSize='small'/>&nbsp;
-              <Typography variant={isMobile ? 'caption' : 'body1'}>
-                <a href="https://twitter.com/intent/tweet" id="tweet-quote" style={{textDecoration: 'none', color: 'white'}}>tweet</a>
-              </Typography>
-            </Button>
-          </Grid>
-          {!isMobile && <Grid item sm={2}/>}
+        <Grid container justifyContent='space-evenly' alignItems='center'>
+          <span className='img-container' onClick={() => void getQuote()}>
+            <img className={clsx(loading && 'disabled')} src="https://www.freeiconspng.com/uploads/thought-icon-bubble-thought-icon-6.png" width={40} alt="new quote" />
+          </span>
+          <a href="https://twitter.com/intent/tweet" className='img-container'>
+            <img src="https://www.freeiconspng.com/uploads/logo-twitter-icon-symbol-0.png" width={55} alt="twitter logo" />
+          </a>
+        </Grid>
       </Grid>
     </div>
   )
